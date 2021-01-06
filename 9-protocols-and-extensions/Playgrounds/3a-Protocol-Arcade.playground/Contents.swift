7ef355f702f7cbd14e24b64d43c987057aa4a216
LeoAOliveira <leonardo.aoliveira@icloud.com>
2021-01-06 16:50:47 -0300

    Protocol and Protocol Delegate Arcade completed

--- 9-protocols-and-extensions/Playgrounds/3a-Protocol-Arcade.playground/Contents.swift
+++ 9-protocols-and-extensions/Playgrounds/3a-Protocol-Arcade.playground/Contents.swift
@@ -1,53 +1,94 @@
 /*
-   ___         _               _   ___      _               _
-  | _ \_ _ ___| |_ ___  __ ___| | |   \ ___| |___ __ _ __ _| |_ ___
-  |  _/ '_/ _ \  _/ _ \/ _/ _ \ | | |) / -_) / -_) _` / _` |  _/ -_)
-  |_| |_| \___/\__\___/\__\___/_| |___/\___|_\___\__, \__,_|\__\___|
-                                                 |___/
-                        _                  _
-                       /_\  _ _ __ __ _ __| |___
-                      / _ \| '_/ _/ _` / _` / -_)
-                     /_/ \_\_| \__\__,_\__,_\___|
+   ___         _               _     _                  _
+  | _ \_ _ ___| |_ ___  __ ___| |   /_\  _ _ __ __ _ __| |___
+  |  _/ '_/ _ \  _/ _ \/ _/ _ \ |  / _ \| '_/ _/ _` / _` / -_)
+  |_| |_| \___/\__\___/\__\___/_| /_/ \_\_| \__\__,_\__,_\___|
+ 
  */
 
 /*
- protocol WeatherServiceDelegate: AnyObject {
-     func didFetchWeather(weather: String)
- }
-
- struct WeatherService {
-     weak var delegate: WeatherServiceDelegate?
-     
-     func fetchWeather() {
-         // asynchronous...
-         delegate?.didFetchWeather(weather: "22")
-     }
- }
-
- override func viewDidLoad() {
-     super.viewDidLoad()
-     weatherService.delegate = self // key!
-     setupViews()
- }
-
- extension ViewController: WeatherServiceDelegate {
-     func didFetchWeather(weather: String) {
-         label.text = weather
-     }
- }
+ 🕹 Movable Player.
  */
 
-/*
-🕹 Death Star
- 
- Using the Protocol Delegate pattern, create a service that informs General Tarkin in the Command Center when the Death Star's primary canon is ready to fire.
- 
- In no particular order you will need to:
-  - Create the protocol.
-  - Create the service.
-  - Create the Command Center.
-  - Register the Command Center delegate with the service.
-  - Make the Command Center conform to the protocol.
-  - Fire the canon! (via a print statement).
+// Given
 
- */
+struct Point {
+    let x: Int
+    let y: Int
+    
+    init(_ x: Int, _ y: Int) {
+        self.x = x
+        self.y = y
+    }
+}
+
+struct Player {
+    var position = Point(0, 0)
+}
+
+// 1. Define an enum called 'Direction' that allows a player to move left, right up and down.
+
+enum Direction {
+    case left
+    case right
+    case up
+    case down
+}
+
+// 2. Create a protocol called 'Moveable' with two methods:
+//    - func move(_ direction: Direction)
+//    - func coordinates() -> String
+
+protocol Moveable {
+    mutating func move(_ direction: Direction)
+    func coordinates() -> String
+}
+
+// 3. Make 'Player' conform to 'Moveable' protocol via a protocol extension
+//    - extension Player: Moveable {}
+//
+//    - make the 'move' function update the players current position by assigning it a new 'Point' based on the direction passed in.
+//    - make the 'coordinates' function return a string of the form (x, y).
+
+
+extension Player: Moveable {
+    
+    mutating func move(_ direction: Direction) {
+        
+        var x: Int = position.x
+        var y: Int = position.y
+        
+        switch direction {
+        case .left:
+            x -= 1
+        case .right:
+            x += 1
+        case .up:
+            y += 1
+        case .down:
+            y -= 1
+        }
+        
+        position = Point(x, y)
+    }
+    
+    func coordinates() -> String {
+        return "(\(position.x), \(position.y))"
+    }
+}
+
+var player1 = Player()
+player1.coordinates()
+
+player1.move(.left)
+player1.coordinates()
+
+player1.move(.right)
+player1.coordinates()
+
+player1.move(.up)
+player1.move(.up)
+player1.coordinates()
+
+player1.move(.down)
+player1.coordinates()
--- 9-protocols-and-extensions/Playgrounds/3b-Protocol-Arcade-Solution.playground/Contents.swift
+++ 9-protocols-and-extensions/Playgrounds/3b-Protocol-Arcade-Solution.playground/Contents.swift
@@ -1,99 +1,92 @@
 /*
-   ___         _               _   ___      _               _
-  | _ \_ _ ___| |_ ___  __ ___| | |   \ ___| |___ __ _ __ _| |_ ___
-  |  _/ '_/ _ \  _/ _ \/ _/ _ \ | | |) / -_) / -_) _` / _` |  _/ -_)
-  |_| |_| \___/\__\___/\__\___/_| |___/\___|_\___\__, \__,_|\__\___|
-                                                 |___/
-                        _                  _
-                       /_\  _ _ __ __ _ __| |___
-                      / _ \| '_/ _/ _` / _` / -_)
-                     /_/ \_\_| \__\__,_\__,_\___|
+   ___         _               _     _                  _
+  | _ \_ _ ___| |_ ___  __ ___| |   /_\  _ _ __ __ _ __| |___
+  |  _/ '_/ _ \  _/ _ \/ _/ _ \ |  / _ \| '_/ _/ _` / _` / -_)
+  |_| |_| \___/\__\___/\__\___/_| /_/ \_\_| \__\__,_\__,_\___|
+ 
  */
 
 /*
- protocol WeatherServiceDelegate: AnyObject {
-     func didFetchWeather(weather: String)
- }
-
- struct WeatherService {
-     weak var delegate: WeatherServiceDelegate?
-     
-     func fetchWeather() {
-         // asynchronous...
-         delegate?.didFetchWeather(weather: "22")
-     }
- }
-
- override func viewDidLoad() {
-     super.viewDidLoad()
-     weatherService.delegate = self // key!
-     setupViews()
- }
-
- extension ViewController: WeatherServiceDelegate {
-     func didFetchWeather(weather: String) {
-         label.text = weather
-     }
- }
+ 🕹 Movable Player.
  */
 
-/*
-🕹 Death Star
- 
- Using the Protocol Delegate pattern, create a service that informs General Tarkin in the Command Center when the Death Star's primary canon is ready to fire.
- 
- In no particular order you will need to:
-  - Create the protocol.
-  - Create the service.
-  - Create the Command Center.
-  - Register the Command Center delegate with the service.
-  - Make the Command Center conform to the protocol.
-  - Fire the canon! (via a print statement).
+// Given
 
- */
-
-import Foundation
-
-protocol DeathStarCanonDelegate {
-    func canonReady()
-}
-
-struct DeathStarCanonService {
-    var delegate: DeathStarCanonDelegate?
+struct Point {
+    let x: Int
+    let y: Int
     
-    func chargePrimaryCanon() {
-        delegate?.canonReady()
+    init(_ x: Int, _ y: Int) {
+        self.x = x
+        self.y = y
     }
 }
 
-struct CommandCenter {
+struct Player {
+    var position = Point(0, 0)
+}
+
+// 1. Define an enum called 'Direction' that allows a player to move left, right up and down.
+
+enum Direction {
+    case left
+    case right
+    case up
+    case down
+}
+
+// 2. Create a protocol called 'Moveable' with two methods:
+//    - func move(_ direction: Direction)
+//    - func coordinates() -> String
+
+protocol Moveable {
+    mutating func move(_ direction: Direction)
+    func coordinates() -> String
+}
+
+// 3. Make 'Player' conform to 'Moveable' protocol via a protocol extension
+//    - extension Player: Moveable {}
+//
+//    - make the 'move' function update the players current position by assigning it a new 'Point' based on the direction passed in.
+//    - make the 'coordinates' function return a string of the form (x, y).
+
+
+extension Player: Moveable {
     
-    var service = DeathStarCanonService()
-    
-    init() {
-        service.delegate = self
+    mutating func move(_ direction: Direction) {
+        
+        let currentX: Int = position.x
+        let currentY: Int = position.y
+        
+        switch direction {
+        case .left:
+            position = Point(currentX - 1, currentY)
+        case .right:
+            position = Point(currentX + 1, currentY)
+        case .up:
+            position = Point(currentX, currentY + 1)
+        case .down:
+            position = Point(currentX, currentY - 1)
+        }
     }
     
-    func fireWhenReady() {
-        service.chargePrimaryCanon()
-    }
-    
-    private func fire() {
-        print("Boom!")
+    func coordinates() -> String {
+        return "(\(position.x), \(position.y))"
     }
 }
 
-extension CommandCenter: DeathStarCanonDelegate {
-    func canonReady() {
-        fire()
-    }
-}
+var player1 = Player()
+player1.coordinates()
 
-let commandCenter = CommandCenter()
-commandCenter.fireWhenReady()
+player1.move(.left)
+player1.coordinates()
 
-/*
- Discussion:
- - weak var delegate only necessary for class
- 
- */
+player1.move(.right)
+player1.coordinates()
+
+player1.move(.up)
+player1.move(.up)
+player1.coordinates()
+
+player1.move(.down)
+player1.coordinates()
